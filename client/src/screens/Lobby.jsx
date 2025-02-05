0d2503a80eb73cc7c836d3bdcb29dff9f2a00768
surajkr20 <suraj71308kumar@gmail.com>
2025-02-05 07:12:25 +0530

    form handling

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -1,13 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { TextField } from '@mui/material';
+import React, { useCallback, useState } from "react";
+import { TextField, Button } from '@mui/material';
 
 const Lobby = () => {
+
+    const [email, setEmail] = useState("");
+    const [room, setRoom] = useState("");
+
+    const submitHandler = useCallback((e)=>{
+        e.preventDefault();
+        console.log(email, room)
+    }, [email, room])
+
   return (
-    <div className="">
-      <h2 className="">Lobby</h2>
-      <form action="">
-        <TextField id="outlined-basic" label="Email" variant="outlined" />
+    <div className="p-8">
+      <h2 className="font-semibold text-2xl">Lobby</h2>
+      <form className="flex flex-row gap-3 mt-2" onSubmit={submitHandler}>
+        <TextField id="outlined-basic" label="Email" variant="outlined" value={email} onChange={(e)=>{
+            setEmail(e.target.value);
+        }}/>
+        <TextField id="outlined-basic" label="Room No" variant="outlined" value={room} onChange={(e)=>{
+            setRoom(e.target.value)
+        }}/>
+        <Button variant="contained" type="submit">Join</Button>
       </form>
     </div>
   );
